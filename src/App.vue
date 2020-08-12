39cd6c92338ca297622f53167036f75eb799acbf
Aymane Benhima <aymanebenhima@gmail.com>
2020-08-12 18:15:06 +0100

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <app-quote />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quote from './components/Quote.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    appQuote: Quote,
+  },
+  data() {
+    return {
+      quotes: [
+        'Test this quote'
+      ],
+      maxQuotes: 10
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
