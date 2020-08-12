5f6fd6d6d89b2ff669fb17c5f096ce71ab33ee66
Aymane Benhima <aymanebenhima@gmail.com>
2020-08-12 23:44:09 +0100

    push quote

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes" />
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
 
 export default {
   components: {
     appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
   },
   data() {
     return {
@@ -17,6 +25,11 @@ export default {
         'Test this quote'
       ],
       maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote)
     }
   },
 }
